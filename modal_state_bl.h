3e59de5d08a2b2ffba607a8026f6ec7eeb29f981
Marek Hyčko <mhycko@gmail.com>
2020-12-15 14:41:30 +0100

    Renaming solution and project.

--- modal_state_bl.h
+++
@@ -1,8 +0,0 @@
-﻿// modal_state_bl.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
